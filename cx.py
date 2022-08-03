301519d8682658e42e780abc3d8c50ebae3eb412
Joel Silva <53149347+zoreu@users.noreply.github.com>
2022-08-03 20:10:11 -0300

    Update cx.py

--- cx.py
+++ cx.py
@@ -1,9 +1,9 @@
 #test deploy
-import os
-try:
-  os.mkdir('build')
-except:
-  pass
-jsonfile = os.path.join('build','cx.json')
-with open(jsonfile, 'w') as f:
+# import os
+# try:
+#   os.mkdir('build')
+# except:
+#   pass
+# jsonfile = os.path.join('build','cx.json')
+with open('cx.json, 'w') as f:
   f.write('teste')
